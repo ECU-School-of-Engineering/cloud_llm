56a72a88152271cfbca032cbb7e90b256bdee0d1
luciferCJ <41362203+luciferCJ@users.noreply.github.com>
2026-03-11 15:02:34 +0800

    deploy 2026-03-11 15:02

--- clm_engine.py
+++ clm_engine.py
@@ -1,6 +1,7 @@
 # clm_engine.py
 # start with:
 # uvicorn clm_engine:app --host 0.0.0.0 --port 8080 --log-level debug
+# Version - 1.00
 
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
